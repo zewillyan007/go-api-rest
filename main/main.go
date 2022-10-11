354706f3c203543a8b4a95561776ee9e508bdb1c
José Willyan <willyansilvagomes2020@gmail.com>
2022-10-11 16:08:03 -0300

    aprendendo docker

--- main/main.go
+++ main/main.go
@@ -10,6 +10,7 @@ func main() {
 	models.Persons = []models.SinglePerson{
 		{Id: 1, Name: "Name 1", Story: "Story 1"},
 		{Id: 2, Name: "Name 2", Story: "Story 2"},
+		{Id: 3, Name: "Name 3", Story: "Story 3"},
 	}
 
 	fmt.Println("Starting Rest Server With Go")
