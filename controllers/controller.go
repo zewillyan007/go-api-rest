46093c417e5818aad58e21c6a935a0cbd788c15d
José Willyan <willyansilvagomes2020@gmail.com>
2022-10-13 14:27:53 -0300

    crud finalizado

--- controllers/controller.go
+++ controllers/controller.go
@@ -28,3 +28,28 @@ func ReturnOnePerson(w http.ResponseWriter, r *http.Request) {
 	database.DB.First(&p, id)
 	json.NewEncoder(w).Encode(p)
 }
+
+func CreateNewPerson(w http.ResponseWriter, r *http.Request) {
+	var NewPersons models.Persons
+	json.NewDecoder(r.Body).Decode(&NewPersons)
+	database.DB.Create(&NewPersons)
+	json.NewEncoder(w).Encode(NewPersons)
+}
+
+func DeletePerson(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+	var OldPerson models.Persons
+	database.DB.Delete(&OldPerson, id)
+	json.NewEncoder(w).Encode(OldPerson)
+}
+
+func EditPerson(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+	var FailPerson models.Persons
+	database.DB.First(&FailPerson, id)
+	json.NewDecoder(r.Body).Decode(&FailPerson)
+	database.DB.Save(&FailPerson)
+	json.NewEncoder(w).Encode(FailPerson)
+}
--- routes/routes.go
+++ routes/routes.go
@@ -12,6 +12,9 @@ func HandleResquest() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", controllers.Home)
 	r.HandleFunc("/api/persons", controllers.AllPersons).Methods("GET")
+	r.HandleFunc("/api/persons", controllers.CreateNewPerson).Methods("POST")
 	r.HandleFunc("/api/persons/{id}", controllers.ReturnOnePerson).Methods("GET")
+	r.HandleFunc("/api/persons/{id}", controllers.DeletePerson).Methods("DELETE")
+	r.HandleFunc("/api/persons/{id}", controllers.EditPerson).Methods("PUT")
 	log.Fatal(http.ListenAndServe(":8000", r))
 }
